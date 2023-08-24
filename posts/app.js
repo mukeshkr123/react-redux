b5cbbcda8fbc5f86b811211a1409fae0338d5e34
Mukesh Kumar <mukeshmehta2041@gmail.com>
2023-08-24 14:32:29 +0530

    configuring store and dispatching action

--- posts/app.js
+++ posts/app.js
@@ -1,4 +1,8 @@
-const { createAsyncThunk } = require("@reduxjs/toolkit");
+const {
+  createAsyncThunk,
+  createSlice,
+  configureStore,
+} = require("@reduxjs/toolkit");
 const { default: axios } = require("axios");
 
 const baseURL = "https://jsonplaceholder.typicode.com/posts";
@@ -12,8 +16,44 @@ const initialState = {
 
 // action AsyncThunk
 const fetchPostAction = createAsyncThunk("posts/fetch", async () => {
-  const data = await axios.get(baseURL);
-  return data;
+  const res = await axios.get(baseURL);
+  return res.data; // grabing the data only
 });
 
 // slice
+const postSlice = createSlice({
+  name: "posts",
+  initialState,
+  extraReducers: (builder) => {
+    //Handle lifecycle - pending-success , rejected
+    // pending
+    builder.addCase(fetchPostAction.pending, (state) => {
+      state.loading = true;
+    });
+    // fullfilled
+    builder.addCase(fetchPostAction.fulfilled, (state, action) => {
+      state.posts = action.payload;
+      state.loading = false;
+    });
+    // rejected
+    builder.addCase(fetchPostAction.rejected, (state) => {
+      state.posts = [];
+      state.loading = false;
+      state.error = action.payload;
+    });
+  },
+});
+
+// generate reducer
+const postsReducer = postSlice.reducer;
+
+// store
+const store = configureStore({
+  reducer: postsReducer,
+});
+
+// dispatch the action
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(fetchPostAction());
